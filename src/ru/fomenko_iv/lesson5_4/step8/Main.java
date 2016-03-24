4f3498657add2686dc10a94509b88989098ccb00
Igor Fomenko <harryfom@gmail.com>
2016-03-24 21:49:06 +0300

    5.4 Продвинутые возможности – Шаг 8 Begin

---
+++ src/ru/fomenko_iv/lesson5_4/step8/Main.java
@@ -0,0 +1,10 @@
+package ru.fomenko_iv.lesson5_4.step8;
+
+/**
+ * Created by dns on 24.03.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" 5.4 Продвинутые возможности – Шаг 8");
+    }
+}
